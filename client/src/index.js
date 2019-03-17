539255ecb3263491ee44c886af38ce9b5cec2740
programmer <programmer@localhost.localdomain>
2019-03-17 19:14:45 +0530

    just add status

--- client/src/index.js
+++ client/src/index.js
@@ -23,4 +23,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-// 14-6 min
+// 21 
